a549ac9c962761505dd2ad15e525be7104b60380
Victoria Chiang <vchiang18@gmail.com>
2024-03-15 14:51:15 -0600

    added user views and log view outline

--- servings/views.py
+++ servings/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Log, Progress
+# from servings.forms import LogForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+def create_log(request):
+    pass
+
+def view_log(request):
+    pass
+
+def edit_log(request):
+    pass
+
+def view_logs(request):
+    pass
+
+def delete_log(request):
+    pass
--- users/views.py
+++ users/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.models import User
+from users.forms import LoginForm, SignupForm
+
 
 # Create your views here.
+def user_login(request):
+    if request.method == "POST":
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = authenticate(
+                request,
+                username=username,
+                password=password
+            )
+            if user is not None:
+                login(request, user)
+                return redirect("home")
+    else:
+        form = LoginForm()
+    context = {
+        "form": form,
+        }
+    return render(request, "users/login.html", context)
+
+
+def user_logout(request):
+    logout(request)
+    return redirect("login")
+
+
+def user_signup(request):
+    if request.method == "POST":
+        form = SignupForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            password_confirmation = form.cleaned_data['password_confirmation']
+
+            if password == password_confirmation:
+                user = User.objects.create_user(
+                    username,
+                    password=password
+                )
+                login(request, user)
+                return redirect("home")
+            else:
+                form.add_error("password", "the passwords do not match")
+    else:
+        form = SignupForm()
+        context = {
+            "form": form,
+        }
+    return render(request, "users/signup.html", context)
