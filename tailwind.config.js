16dd5299b3769b1761e5ddab28f700a5eadd92aa
Victoria Chiang <vchiang18@gmail.com>
2024-05-02 12:16:24 -0700

    tailwind connected

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,28 @@
+// // docker directions
+// /** @type {import('tailwindcss').Config} */
+// module.exports = {
+//   purge: [],
+//   darkMode: false, // or 'media' or 'class'
+//   theme: {
+//     extend: {},
+//   },
+//   variants: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+// venv directions
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
+  content: [
+    // './app-name/templates/app-name/**/*.html',
+    './servings/templates/servings/**/*.html',
+    './users/templates/registration/**/*.html',
+    // Add paths to other apps if necessary
+  ],
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
   plugins: [],
-}
+};
