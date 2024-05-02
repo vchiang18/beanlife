dd3df9187b0fd5d3475dff080cec906d96dbc200
Victoria Chiang <vchiang18@gmail.com>
2024-05-01 17:38:50 -0700

    progress bar progress

--- static/servings/index.js
+++ static/servings/index.js
@@ -1,1 +1,21 @@
 // alert("index.js hooked up!");
+
+let circularProgress = document.querySelector(".circular-progress");
+let progressValue = document.querySelector(".progress-value");
+console.log(progressValue);
+
+
+let progressStartValue = 0,
+    progressEndValue = 100,
+    speed = 80;
+
+let progress = setInterval( () => {
+    progressStartValue++;
+    progressValue.textContent = `${progressStartValue}%`
+    circularProgress.style.background = `conic-gradient(#eea516 ${progressStartValue *3.6}deg, #E4D6C4 0deg)`
+
+    if(progressStartValue == progressEndValue) {
+        clearInterval(progress);
+    }
+    // console.log(progressStartValue);
+}, speed);
